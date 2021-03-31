a1ee6f61a404c7b6d1f03cdfd77e21a745660d0c
Esteban Solano Granados <stvansolano@outlook.com>
2021-03-31 05:40:27 +0000

    Added ServiceB

---
+++ src/services/ServiceB/Program.cs
@@ -0,0 +1,24 @@
+﻿// Program
+
+using System.IO;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Primitives;
+
+class Program
+{
+    public static void Main(string[] args) =>  WebHost.CreateDefaultBuilder(args)
+    .Configure(app =>
+    {
+        app.UseRouting();
+        app.UseEndpoints(e =>
+        {
+            e.MapGet("/", context =>
+                context.Response.WriteAsJsonAsync(new { Message = "Hello, World! This is Service B"})
+            );
+        });
+    }).Build().Run();
+}
